df37b5a46d87d938fc4a1c758e8739d5ac70f02b
Ahmad Tufail <52331077+ahmadkhudai@users.noreply.github.com>
2021-12-24 20:50:40 +0500

    month/week switch added.

--- pages/Home/HomePage.tsx
+++ pages/Home/HomePage.tsx
@@ -176,7 +176,7 @@ export function HomePage({switchWindow}: Props) {
 
 
 
-                    <div className={"py-4"}>
+                    <div className={"py-4 w-100"}>
                         <div>
                             <h1 className={"h3 text-center w-auto "}>Expense Tracker</h1>
                             <h3 className={"ak_accent_text text-center font-monospace w-auto "}>Your {viewMode===ViewModes.today?"day":viewMode} so far...</h3>
