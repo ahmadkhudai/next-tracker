ad5766ed57989a0ae008b2e9dafc407eeb4d1db6
Ahmad Tufail <52331077+ahmadkhudai@users.noreply.github.com>
2022-01-05 00:21:09 +0500

    sample data removal added

--- pages/add_expense/_components/_Non_DateInputs/InputComponent.tsx
+++ pages/add_expense/_components/_Non_DateInputs/InputComponent.tsx
@@ -20,23 +20,28 @@ type Props = {
 export function InputComponent({selfRef, moveToNextInput,title, subtitle, handleFieldChange, newExpense, nextInputRef, fieldLabel, fieldType}: Props) {
     return (
         <>
-            <label htmlFor={fieldLabel} className={"w-100"} ref={nextInputRef}>
-                <LargeFormDisplay content={title} styleClasses={"p-2   font-thin  unselectable"}/>
-                <p className={"text-sm text-center text-gray-400"}>{subtitle}</p>
-            </label>
+            {newExpense && nextInputRef &&
+            <>
+                <label htmlFor={fieldLabel} className={"w-100"} ref={nextInputRef}>
+                    <LargeFormDisplay content={title} styleClasses={"p-2   font-thin  unselectable"}/>
+                    <p className={"text-sm text-center text-gray-400"}>{subtitle}</p>
+                </label>
 
-            <input
-                   ref={selfRef}
+                <input
+                    ref={selfRef}
                     type={fieldType}
-                   className="form-control border-0 text-center   focus:bg-purple-500 focus:text-white focus:font-bold hover:text-[1.3rem]"
-                   id={fieldLabel} placeholder={fieldLabel}
+                    className="form-control border-0 text-center   focus:bg-purple-500 focus:text-white focus:font-bold hover:text-[1.3rem]"
+                    id={fieldLabel} placeholder={fieldLabel}
                     onKeyUp={(e)=> {
                         moveToNextInput(e, nextInputRef);
                     }}
-                   /* @ts-ignore*/
-                   value={newExpense[fieldLabel]} onChange={e => {
-                handleFieldChange(fieldLabel, e.target.value)
-            }} />
+                    /* @ts-ignore*/
+                    value={newExpense[fieldLabel]} onChange={e => {
+                    handleFieldChange(fieldLabel, e.target.value)
+                }} />
+            </>
+            }
+
         </>
     );
 }
