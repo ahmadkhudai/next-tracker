30708827e75ceddea6d8313a005980732090304a
Ahmad Tufail <52331077+ahmadkhudai@users.noreply.github.com>
2022-01-22 15:58:31 +0500

    brought homepage down to 190

--- pages/Home/HomePage.tsx
+++ pages/Home/HomePage.tsx
@@ -1,51 +1,20 @@
 // @flow
 import React, {useEffect, useState} from 'react';
 import {SettingsObj} from "../../Definitions/SettingsObj";
-import {Expense, RequiredFields} from "../../Definitions/Expense";
+import {Expense} from "../../Definitions/Expense";
 import HomeHeader from "../components/HomeHeader/HomeHeader";
-import Link from "next/link";
-import {
-    baseSettings,
-    getCurrentMonthsExpenses,
-    currentWeekGraphables,
-    getRenderableCurrentMONTHsExpenses,
-    getRenderableCurrentWeeksExpenses,
-    getRenderableTODAYsExpenses,
-    getSortedExpenses,
-    getTodaysExpenses,
-    sortfunction,
-    sumAllExpenses
-} from "../../libs/utils/expense/grouping";
+import {baseSettings} from "../../libs/utils/expense/grouping";
 import {v4 as uuidv4} from "uuid";
 import {addDays, isGreaterThanToday} from "../../libs/utils/date_utils";
-import {OptionPanelLabels, OptionsPanels} from "../../libs/component_config/Main/OptionsPanels";
+import {OptionsPanels} from "../../libs/component_config/Main/OptionsPanels";
 import AK_SettingsPanel from "../Forms/AK_SettingsPanel";
 import AddExpenseForm from "../add_expense/AddExpenseForm";
 import ModalContainer from "../Framer/ModalContainer";
-import HomeFooter from "../components/HomeFooter";
-import NoData from "../components/_partials/NoData";
-import {HomePanelLabels, HomePanels} from "../../libs/component_config/HomePanels";
-import CurrentVisual from "./_components/CurrentVisual";
-import {dateFunctions, ViewModes} from "../../libs/component_config/ViewModes";
-import {Day} from "../../constants/day";
+import {ViewModes} from "../../libs/component_config/ViewModes";
 import Backdrop from "../Framer/Backdrop";
-import ViewModeButtons from "./_components/ViewModeButtons";
-import HomeExpensesView from "./_components/compound_components/HomeExpensesView";
-import HomeStats from "./_components/HomeStats";
-import {dumdumData} from "../../libs/dummy_data/data";
-import {nFormatter, removeArrIndexes} from "../../libs/utils/num_utils";
 //@ts-ignore
 import * as XLSX from 'xlsx';
-import PurpleButton from "../components/buttons/PurpleButton";
-import {readDataSheet, saveExpenses} from "../../Exellent/main";
-import {hasRequiredProps} from "../../Exellent/validator";
-import moment from "moment";
-import saveIcon from '../../assets/save.png';
-import loadIcon from '../../assets/upload.png';
-import Image from 'next/image';
-import {SettingLabels} from "../../Definitions/Setting";
 import DownloadForm from "./_components/DownloadForm";
-import {repairExpenseAmounts} from "../../libs/utils/expense/repair";
 import {removeSampleData} from "../../libs/utils/expense/clearing";
 import {loadExpenses, loadSettings, modifyExpenses} from "../../libs/pages/_common/loaders";
 import MainView from "./_partials/main_view";
@@ -53,9 +22,6 @@ import MainView from "./_partials/main_view";
 type Props = {
     switchWindow: any;
 };
-type State = {};
-const { networkInterfaces } = require('os');
-
 
 
 export function HomePage({switchWindow}: Props) {
@@ -64,25 +30,21 @@ export function HomePage({switchWindow}: Props) {
     let loadedExpenses: Expense[] = [];
     let loadedSettings: SettingsObj = baseSettings;
     const [expenses, setExpenses] = useState(loadedExpenses);
-    const [currentExpenses, setCurrentExpenses] = useState([] as Expense[]);
     const [settings, setSettings] = useState(loadedSettings);
     const [sampleDataExists, setSampleDataExists] = useState(true);
 
     //visual state
-    const [graphAbleExpenses, setGraphAbleExpenses] = useState([] as Expense[]);
     const [currentlyOpenPanel, setCurrentlyOpenPanel] = useState(OptionsPanels.none);
-    const [currentHomePanel, setCurrentHomePanel] = useState(HomePanels.none);
+
 
     const [viewMode, setViewMode] = useState(ViewModes.week);
     const [successMessage, setSuccessMessage] = useState(null as any);
 
 
     useEffect(() => {
-        modifyExpenses(loadExpenses(),setExpenses);
+        modifyExpenses(loadExpenses(), setExpenses);
         modifySettings(loadSettings());
     }, []);
-
-
 
 
     function modifySettings(modifiedSettings: SettingsObj) {
@@ -116,14 +78,12 @@ export function HomePage({switchWindow}: Props) {
     }
 
 
-
     function deleteExpense(toDelete: Expense) {
 
         let newExpenseList = expenses.filter((expense: Expense) => expense.id !== toDelete.id);
         modifyExpenses(newExpenseList, setExpenses);
 
     }
-
 
 
     function openPanel(panel: OptionsPanels) {
@@ -141,55 +101,6 @@ export function HomePage({switchWindow}: Props) {
         }
     }
 
-    function openHomePanel(panel: HomePanels) {
-        setCurrentHomePanel(panel);
-
-    }
-
-    //react to change
-    useEffect(() => {
-        let stateUpdated = false;
-        let tempgraphAbleExpenses: any = [];
-        let tempcurrentExpenses: any = [];
-        if (expenses.length >= 1) {
-            if (viewMode === ViewModes.today) {
-                tempcurrentExpenses = getRenderableTODAYsExpenses(getSortedExpenses(expenses));
-                tempgraphAbleExpenses = getTodaysExpenses(getSortedExpenses(expenses));
-
-                stateUpdated = true;
-            }
-            if (viewMode === ViewModes.month) {
-                tempcurrentExpenses = getRenderableCurrentMONTHsExpenses(getSortedExpenses(expenses));
-                tempgraphAbleExpenses = getCurrentMonthsExpenses(getSortedExpenses(expenses));
-
-                stateUpdated = true;
-
-            }
-            if (viewMode === ViewModes.week) {
-                tempcurrentExpenses = getRenderableCurrentWeeksExpenses(getSortedExpenses(expenses));
-                tempgraphAbleExpenses = currentWeekGraphables(getSortedExpenses(expenses));
-                stateUpdated = true;
-            }
-            if (stateUpdated) {
-                setCurrentExpenses(tempcurrentExpenses);
-                setGraphAbleExpenses(tempgraphAbleExpenses);
-                if (tempcurrentExpenses.length == 0) {
-                    openHomePanel(HomePanels.none);
-                } else if (tempgraphAbleExpenses.length > 1) {
-                    if (currentHomePanel === HomePanels.none) {
-                        openHomePanel(HomePanels.Visualize);
-                    }
-                } else {
-                    openHomePanel(HomePanels.ExpensesPanel);
-                }
-            }
-
-        } else {
-            setGraphAbleExpenses([]);
-            setCurrentExpenses([]);
-        }
-    }, [expenses, viewMode]);
-
 
     function updateViewMode(newMode: ViewModes) {
 
@@ -198,21 +109,6 @@ export function HomePage({switchWindow}: Props) {
         }
 
     }
-
-    function getGraphY(viewMode: ViewModes) {
-        if (viewMode == ViewModes.today) {
-            return "Hour"
-        }
-
-        return "Day"
-    }
-
-
-
-
-
-
-
 
 
     return (
@@ -253,40 +149,21 @@ export function HomePage({switchWindow}: Props) {
                     <div className={" flex items-center flex-column justify-center ak_max_600px w-100 "}>
 
 
-                        <div className={"py-2   w-100 h-100"}>
-                            <div>
+                        <MainView openPanel={openPanel} updateViewMode={updateViewMode}
+                                  currentlyOpenPanel={currentlyOpenPanel} settings={settings} expenses={expenses}
+                                  deleteExpense={deleteExpense} viewMode={viewMode}/>
 
-                                <h3 className={"text-teal-500 text-center text-xl font-monospace w-auto "}>{nFormatter(currentExpenses.reduce(sumAllExpenses, 0))} spent {viewMode !== ViewModes.today ? "this " : ""}
-                                    <span
-                                        className={"text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-teal-600 "}>{viewMode}</span>
-                                </h3>
-                            </div>
-                            <div className={"w-100 ak_max_600px flex align-items-center justify-content-center  mt-1"}>
-                                <ViewModeButtons currentViewMode={viewMode} updateViewMode={updateViewMode}/>
-                            </div>
-
-                            {currentHomePanel === HomePanels.Home &&
-                                <div className={" ak_max_600px w-100 h-full"}>
-
-                                    <div className={"h-auto "}>
-                                        <HomeStats settings={settings} viewMode={viewMode} expenses={currentExpenses}/>
-                                    </div>
-                                </div>
-                            }
-                            <MainView currentHomePanel={currentHomePanel} settings={settings} graphAbleExpenses={graphAbleExpenses} currentExpenses={currentExpenses} deleteExpense={deleteExpense} viewMode={viewMode}/>
-                        </div>
                         {currentlyOpenPanel === OptionsPanels.DownloadUploadForm &&
                             <Backdrop onClick={(e: any) => {
                                 closeAllPanels(e)
                             }}>
-                            <DownloadForm setExpenses={setExpenses} closeAllPanels={closeAllPanels} setSuccessMessage={setSuccessMessage} modifyExpenses={modifyExpenses} expenses={expenses}/>
+                                <DownloadForm setExpenses={setExpenses} closeAllPanels={closeAllPanels}
+                                              setSuccessMessage={setSuccessMessage} modifyExpenses={modifyExpenses}
+                                              expenses={expenses}/>
                             </Backdrop>
                         }
 
                     </div>
-                }
-                {currentHomePanel === HomePanels.none && currentlyOpenPanel !== OptionsPanels.AddExpensePanel &&
-                    <NoData customMessage={"Nothing to show here."}/>
                 }
 
 
@@ -313,26 +190,6 @@ export function HomePage({switchWindow}: Props) {
 
 
             </div>
-            {currentlyOpenPanel !== OptionsPanels.AddExpensePanel &&
-                <HomeFooter
-                    currentHomePanel={currentHomePanel}
-                    openHomePanelFunc={openHomePanel}
-                    homePanels={graphAbleExpenses.length > 1 ? [{
-                        panelLabel: HomePanelLabels.ExpensesPanel,
-                        panel: HomePanels.ExpensesPanel
-                    },
-                        {panelLabel: HomePanelLabels.Visualize, panel: HomePanels.Visualize},
-                        {panelLabel: HomePanelLabels.Home, panel: HomePanels.Home}
-                    ] : []
-                    }
-                    addButton={
-                        {
-                            panelLabel: OptionPanelLabels.AddExpensePanel,
-                            panel: OptionsPanels.AddExpensePanel
-                        }}
-                    openOptionsPanelFunc={openPanel}
-                />
-            }
         </div>
 
 
--- pages/Home/_partials/main_view.tsx
+++ pages/Home/_partials/main_view.tsx
@@ -1,24 +1,91 @@
 // @flow
 import * as React from 'react';
-import {HomePanels} from "../../../libs/component_config/HomePanels";
+import {HomePanelLabels, HomePanels} from "../../../libs/component_config/HomePanels";
 import CurrentVisual from "../_components/CurrentVisual";
 import {SettingLabels} from "../../../Definitions/Setting";
 import {dateFunctions, ViewModes} from "../../../libs/component_config/ViewModes";
 import HomeExpensesView from "../_components/compound_components/HomeExpensesView";
 import {SettingsObj} from "../../../Definitions/SettingsObj";
 import {Expense} from "../../../Definitions/Expense";
+import {useEffect, useState} from "react";
+import {
+    currentWeekGraphables,
+    getCurrentMonthsExpenses,
+    getRenderableCurrentMONTHsExpenses, getRenderableCurrentWeeksExpenses,
+    getRenderableTODAYsExpenses,
+    getSortedExpenses,
+    getTodaysExpenses, sumAllExpenses
+} from "../../../libs/utils/expense/grouping";
+import {OptionPanelLabels, OptionsPanels} from "../../../libs/component_config/Main/OptionsPanels";
+import HomeFooter from "../../components/HomeFooter";
+import {nFormatter} from "../../../libs/utils/num_utils";
+import ViewModeButtons from "../_components/ViewModeButtons";
+import HomeStats from "../_components/HomeStats";
+import NoData from "../../components/_partials/NoData";
 
 type Props = {
-    currentHomePanel:HomePanels;
+    // currentHomePanel:HomePanels;
+    currentlyOpenPanel:OptionsPanels;
     settings:SettingsObj;
-    graphAbleExpenses:any;
-    currentExpenses:Expense[];
+    // graphAbleExpenses:any;
+    expenses:Expense[];
     deleteExpense:any;
     viewMode:ViewModes;
+    openPanel:any;
+    updateViewMode:any;
 
 };
 
-export function MainView({currentHomePanel, settings,graphAbleExpenses,currentExpenses,deleteExpense, viewMode}: Props) {
+export function MainView({openPanel,updateViewMode, currentlyOpenPanel, settings,expenses,deleteExpense, viewMode}: Props) {
+    const [currentExpenses, setCurrentExpenses] = useState([] as Expense[]);
+    const [graphAbleExpenses, setGraphAbleExpenses] = useState([] as Expense[]);
+    const [currentHomePanel, setCurrentHomePanel] = useState(HomePanels.none);
+    function openHomePanel(panel: HomePanels) {
+        setCurrentHomePanel(panel);
+
+    }
+    useEffect(() => {
+        let stateUpdated = false;
+        let tempgraphAbleExpenses: any = [];
+        let tempcurrentExpenses: any = [];
+        if (expenses.length >= 1) {
+            if (viewMode === ViewModes.today) {
+                tempcurrentExpenses = getRenderableTODAYsExpenses(getSortedExpenses(expenses));
+                tempgraphAbleExpenses = getTodaysExpenses(getSortedExpenses(expenses));
+
+                stateUpdated = true;
+            }
+            if (viewMode === ViewModes.month) {
+                tempcurrentExpenses = getRenderableCurrentMONTHsExpenses(getSortedExpenses(expenses));
+                tempgraphAbleExpenses = getCurrentMonthsExpenses(getSortedExpenses(expenses));
+
+                stateUpdated = true;
+
+            }
+            if (viewMode === ViewModes.week) {
+                tempcurrentExpenses = getRenderableCurrentWeeksExpenses(getSortedExpenses(expenses));
+                tempgraphAbleExpenses = currentWeekGraphables(getSortedExpenses(expenses));
+                stateUpdated = true;
+            }
+            if (stateUpdated) {
+                setCurrentExpenses(tempcurrentExpenses);
+                setGraphAbleExpenses(tempgraphAbleExpenses);
+                if (tempcurrentExpenses.length == 0) {
+                    openHomePanel(HomePanels.none);
+                } else if (tempgraphAbleExpenses.length > 1) {
+                    if (currentHomePanel === HomePanels.none) {
+                        openHomePanel(HomePanels.Visualize);
+                    }
+                } else {
+                    openHomePanel(HomePanels.ExpensesPanel);
+                }
+            }
+
+        } else {
+            setGraphAbleExpenses([]);
+            setCurrentExpenses([]);
+        }
+    }, [expenses, viewMode]);
     function getGraphY(viewMode: ViewModes) {
         if (viewMode == ViewModes.today) {
             return "Hour"
@@ -28,6 +95,28 @@ export function MainView({currentHomePanel, settings,graphAbleExpenses,currentEx
     }
     return (
        <>
+       <div className={"py-2   w-100 h-100"}>
+           <div>
+
+               <h3 className={"text-teal-500 text-center text-xl font-monospace w-auto "}>{nFormatter(currentExpenses.reduce(sumAllExpenses, 0))} spent {viewMode !== ViewModes.today ? "this " : ""}
+                   <span
+                       className={"text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-teal-600 "}>{viewMode}</span>
+               </h3>
+           </div>
+           <div className={"w-100 ak_max_600px flex align-items-center justify-content-center  mt-1"}>
+               <ViewModeButtons currentViewMode={viewMode} updateViewMode={updateViewMode}/>
+           </div>
+
+           {currentHomePanel === HomePanels.Home &&
+               <div className={" ak_max_600px w-100 h-full"}>
+
+                   <div className={"h-auto "}>
+                       <HomeStats settings={settings} viewMode={viewMode} expenses={currentExpenses}/>
+                   </div>
+               </div>
+           }
+
+           </div>
            {currentHomePanel === HomePanels.Visualize &&
                <div className={" ak_max_600px w-100 h-full"}>
 
@@ -51,6 +140,29 @@ export function MainView({currentHomePanel, settings,graphAbleExpenses,currentEx
                </>
 
            }
+           {/*{currentlyOpenPanel !== OptionsPanels.AddExpensePanel &&*/}
+               <HomeFooter
+                   currentHomePanel={currentHomePanel}
+                   openHomePanelFunc={openHomePanel}
+                   homePanels={graphAbleExpenses.length > 1 ? [{
+                       panelLabel: HomePanelLabels.ExpensesPanel,
+                       panel: HomePanels.ExpensesPanel
+                   },
+                       {panelLabel: HomePanelLabels.Visualize, panel: HomePanels.Visualize},
+                       {panelLabel: HomePanelLabels.Home, panel: HomePanels.Home}
+                   ] : []
+                   }
+                   addButton={
+                       {
+                           panelLabel: OptionPanelLabels.AddExpensePanel,
+                           panel: OptionsPanels.AddExpensePanel
+                       }}
+                   openOptionsPanelFunc={openPanel}
+               />
+           {currentHomePanel === HomePanels.none && currentlyOpenPanel !== OptionsPanels.AddExpensePanel &&
+               <NoData customMessage={"Nothing to show here."}/>
+           }
+           {/*}*/}
        </>
     );
 }
